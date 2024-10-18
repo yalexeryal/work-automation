fe90fe93ecb32334849da91bc82ac798b8fc393c
yalexer <yalexer@yandex.ru>
2024-10-18 09:54:47 +0300

    save changes to 18.10.2024

--- db/start_models.py
+++ db/start_models.py
@@ -1,8 +1,7 @@
-models = ['bafe', 'bjs', 'bphp','dtk', 'fpymq', 'git', 'info-an', 'info-btrx', 'info-cpp',
+models = ['bafe', 'bjs', 'bphp', 'dtk', 'fpymq', 'git', 'info-an', 'info-btrx', 'info-cpp',
           'info-fops', 'info-fsmid', 'info-ios', 'info-kk', 'info-ntw', 'info-onec', 'info-onecmid', 'info-pae',
           'info-shdevops', 'info-shfe', 'info-sib', 'info-spd', 'info-sys',
-          'java', 'jdptl', 'jdpts', 'ndjs', 'ndse', 'info-pd', 'py', 'qaptl', 'ra', 'faqa', 'info-spd',
-          'info-ds', 'info-dsu', 'pyda', 'info-aml', 'info-pal', 'info-sal', 'info-awh', 'info-sup', 'sql',
+          'java', 'jdptl', 'jdpts', 'ndjs', 'info-pd', 'py', 'qaptl', 'ra', 'faqa', 'info-spd',
+          'info-ds', 'info-dsu', 'pyda', 'info-aml', 'info-sal', 'info-awh', 'info-sup', 'sql',
           'sqlp', 'info-abi', 'info-abu', 'pbi', 'pdx', 'info-dmar', 'info-ban', 'info- banpro', 'info-da', 'info-dau',
-          'info-deg', 'info-oca', 'info-ocamid', 'banz', 'webpy', 'numpy', 'stpy', 'pyda-diplom', 'dfd', 'tab',
-          'dll', 'feml']
+          'info-oca', 'info-ocamid', 'banz', 'webpy', 'info-fsmidjs']
